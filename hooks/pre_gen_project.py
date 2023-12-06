df01c3afd90af1b9b3ab3a3cc77e03f04d4c5a57
sebas <sla_k89@hotmail.com>
2023-12-06 12:22:19 -0600

    cambio de mensajes y .gitignore

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -8,9 +8,9 @@ MESSAGE_COLOR = "\x1b[34m"
 RESET_ALL = "\x1b[0m"
 
 if project_slug.startswith("x"):
-    print(f'{ERROR_COLOR}ERROR: {project_slug=} is not a valid name for this template.{RESET_ALL}')
+    print(f'{ERROR_COLOR}ERROR: {project_slug=} Lo siento, esto no es un nombre valido para la carpeta.{RESET_ALL}')
 
     sys.exit(1)
 
-print(f"{MESSAGE_COLOR}Let's do it! You're going to create something awesome!")
-print(f"Creating project at { os.getcwd() }{RESET_ALL}")
+print(f"{MESSAGE_COLOR}Listo, empecemos a crear un nuevo proyecto! un paso mas a ser el mejor")
+print(f"creando proyecto en { os.getcwd() }{RESET_ALL}")
